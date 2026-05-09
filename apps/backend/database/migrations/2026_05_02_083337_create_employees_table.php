6a63591645f1f36ef596589230c7af78ed615805
CIITSHSDTP <ciitshsdtp30@ciit.edu.ph>
2026-05-09 12:04:17 +0800

    add controller and models + migration

--- apps/backend/database/migrations/2026_05_02_083337_create_employees_table.php
+++ apps/backend/database/migrations/2026_05_02_083337_create_employees_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('employee', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('email', 50)->unique();
             $table->string('last_name', 100);
